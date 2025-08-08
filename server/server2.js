5d80ce0fb7084bce3371a7ad7be32a91b98f5ee7
Naman Jain <namanjain2004.in@gmail.com>
2025-08-08 10:39:39 +0530

    Commit

---
+++ server/server2.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser'); // Optional if using express built-in middleware
+
+const app = express();
+const port =4004;
+
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // For parsing application/json
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
